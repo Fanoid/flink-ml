ee1c1806157a2a42e3cb11517fccd0f1682e80a4
weibo.zwb <weibo.zwb@alibaba-inc.com>
2023-06-06 17:09:04 +0800

    Merge request from 予何
    * bugfix of type transform for Timestamp to string
    Link: https://code.alibaba-inc.com/PAI/flink-ml/codereview/12864217
    * add paiModelServable API
    
    * Merge branch 'release-2.2-1.0' of http://gitlab.alibaba-inc.com/PAI/flink-ml into release-2.2-1.0
    
    * bugfix of type transform for Timestamp to string
    
    * bugfix of type transform for Timestamp to string
    
    * bugfix of type transform for Timestamp to string
    
    * bugfix of type transform for Timestamp to string
    
    * bugfix of type transform for Timestamp to string

--- flink-ml-lib/src/main/java/org/apache/flink/ml/feature/transform/TypeTransform.java
+++ flink-ml-lib/src/main/java/org/apache/flink/ml/feature/transform/TypeTransform.java
@@ -29,18 +29,25 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.table.api.ApiExpression;
 import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.Expressions;
+import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.api.internal.TableImpl;
 import org.apache.flink.table.catalog.ResolvedSchema;
+import org.apache.flink.table.types.DataType;
 import org.apache.flink.types.Row;
 import org.apache.flink.util.Preconditions;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
 
 /** A Transformer that transform the types of special columns. */
 public class TypeTransform
@@ -58,7 +65,7 @@ public class TypeTransform
         StreamTableEnvironment tEnv =
                 (StreamTableEnvironment) ((TableImpl) inputs[0]).getTableEnvironment();
         ResolvedSchema schema = inputs[0].getResolvedSchema();
-        String[] allCols = schema.getColumnNames().toArray(new String[0]);
+        List<String> allCols = schema.getColumnNames();
         List<String> toDoubleCols = Arrays.asList(getToDoubleCols());
         List<String> toFloatCols = Arrays.asList(getToFloatCols());
         List<String> toIntCols = Arrays.asList(getToIntCols());
@@ -71,9 +78,23 @@ public class TypeTransform
         final int defaultIntVal = getDefaultIntValue();
         final String defaultStringVal = getDefaultStringValue();
 
+        Set<String> toCols = new HashSet<>();
+        Set<String> allColsSet = new HashSet<>(allCols);
+        toCols.addAll(toDoubleCols);
+        toCols.addAll(toFloatCols);
+        toCols.addAll(toIntCols);
+        toCols.addAll(toLongCols);
+        toCols.addAll(toStringCols);
+        for (String col : toCols) {
+            if (!allColsSet.contains(col)) {
+                throw new IllegalArgumentException(
+                        "Column: " + col + " doesn't exist in the input table.");
+            }
+        }
+
         ApiExpression[] expressions =
                 new ApiExpression
-                        [allCols.length
+                        [allCols.size()
                                 + (keepOrigin
                                         ? (toDoubleCols.size()
                                                 + toLongCols.size()
@@ -110,10 +131,25 @@ public class TypeTransform
                                 .tryCast(DataTypes.BIGINT())
                                 .as((keepOrigin ? PREFIX : "") + colName);
             } else if (toStringCols.contains(colName)) {
-                expressions[iter++] =
-                        Expressions.$(colName)
-                                .tryCast(DataTypes.STRING())
-                                .as((keepOrigin ? PREFIX : "") + colName);
+                String typeString = schema.getColumn(colName).get().getDataType().toString();
+                if (typeString.toUpperCase().contains("TIMESTAMP")) {
+                    TimeZone tz = TimeZone.getDefault();
+                    String timeZone =
+                            ((tz.getRawOffset() / 3600000 > 9) ? "GMT-" : "GMT-0")
+                                    + tz.getRawOffset() / 3600000
+                                    + ":00";
+                    tEnv.getConfig().set("table.local-time-zone", timeZone);
+
+                    expressions[iter++] =
+                            Expressions.$(colName)
+                                    .tryCast(DataTypes.TIMESTAMP_LTZ())
+                                    .as((keepOrigin ? PREFIX : "") + colName);
+                } else {
+                    expressions[iter++] =
+                            Expressions.$(colName)
+                                    .tryCast(DataTypes.STRING())
+                                    .as((keepOrigin ? PREFIX : "") + colName);
+                }
             } else {
                 if (!keepOrigin) {
                     expressions[iter++] = Expressions.$(colName);
@@ -124,7 +160,6 @@ public class TypeTransform
         Table middleTable = inputs[0].select(expressions);
         DataStream<Row> outputStream = tEnv.toDataStream(middleTable);
         RowTypeInfo inputTypeInfo = TableUtils.getRowTypeInfo(middleTable.getResolvedSchema());
-
         outputStream =
                 outputStream.map(
                         (MapFunction<Row, Row>)
@@ -157,16 +192,36 @@ public class TypeTransform
                                         } else if (toStringCols.contains(colName)) {
                                             String tmpName =
                                                     keepOrigin ? PREFIX + colName : colName;
-                                            if (row.getField(tmpName) == null) {
+                                            Object obj = row.getField(tmpName);
+                                            if (obj == null) {
                                                 row.setField(tmpName, defaultStringVal);
+                                            } else if (obj instanceof Instant) {
+                                                String ret = obj.toString().replace('T', ' ');
+                                                ret = ret.substring(0, ret.length() - 1);
+                                                row.setField(tmpName, ret);
                                             }
                                         }
                                     }
-
                                     return row;
                                 },
                         inputTypeInfo);
-        Table outputTable = tEnv.fromDataStream(outputStream);
+
+        List<String> names = middleTable.getResolvedSchema().getColumnNames();
+        List<DataType> types = middleTable.getResolvedSchema().getColumnDataTypes();
+        List<String> tmpToStringCols = new ArrayList<>();
+        for (int i = 0; i < toStringCols.size(); ++i) {
+            tmpToStringCols.add(i, keepOrigin ? PREFIX + toStringCols.get(i) : toStringCols.get(i));
+        }
+        for (int i = 0; i < names.size(); ++i) {
+            if (tmpToStringCols.contains(names.get(i))) {
+                types.set(i, DataTypes.STRING());
+            }
+        }
+
+        ResolvedSchema resolvedSchema = ResolvedSchema.physical(names, types);
+        Schema outputSchema = Schema.newBuilder().fromResolvedSchema(resolvedSchema).build();
+        Table outputTable = tEnv.fromDataStream(outputStream, outputSchema);
+
         return new Table[] {outputTable};
     }
 
--- flink-ml-lib/src/test/java/org/apache/flink/ml/feature/TypeTransformTest.java
+++ flink-ml-lib/src/test/java/org/apache/flink/ml/feature/TypeTransformTest.java
@@ -18,6 +18,9 @@
 
 package org.apache.flink.ml.feature;
 
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.typeutils.RowTypeInfo;
 import org.apache.flink.ml.feature.transform.TypeTransform;
 import org.apache.flink.ml.util.TestUtils;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -27,13 +30,16 @@ import org.apache.flink.test.util.AbstractTestBase;
 import org.apache.flink.types.Row;
 
 import org.apache.commons.collections.IteratorUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -309,5 +315,59 @@ public class TypeTransformTest extends AbstractTestBase {
                 Arrays.asList(Row.of(1.0, 3.4028234663852886E38), Row.of(2.0, 1000.0));
         compareResultCollections(
                 expectedRows, collectedResult, Comparator.comparingInt(Row::hashCode));
+    }
+
+    @Test
+    public void testDateToString() throws Exception {
+        List<Row> binomialTrainData =
+                Collections.singletonList(Row.of(1.0, new Timestamp(1685937761202L)));
+        Table inputTable =
+                tEnv.fromDataStream(
+                        env.fromCollection(
+                                binomialTrainData,
+                                new RowTypeInfo(
+                                        new TypeInformation[] {Types.DOUBLE, Types.SQL_TIMESTAMP},
+                                        new String[] {"f1", "f2"})));
+        TypeTransform transform =
+                new TypeTransform()
+                        .setToStringCols("f1", "f2")
+                        .setKeepOldCols(true)
+                        .setDefaultStringValue("null");
+        Table output = transform.transform(inputTable)[0];
+        List<Row> collectedResult =
+                IteratorUtils.toList(tEnv.toDataStream(output).executeAndCollect());
+        List<Row> expectedRows =
+                Collections.singletonList(
+                        Row.of(
+                                1.0,
+                                new Timestamp(1685937761202L),
+                                "1.0",
+                                "2023-06-05 20:02:41.202"));
+        compareResultCollections(
+                expectedRows, collectedResult, Comparator.comparingInt(Row::hashCode));
+    }
+
+    @Test
+    public void testErrorCols() throws Exception {
+        List<Row> binomialTrainData =
+                Collections.singletonList(Row.of(1.0, new Timestamp(1685937761202L)));
+        Table inputTable =
+                tEnv.fromDataStream(
+                        env.fromCollection(
+                                binomialTrainData,
+                                new RowTypeInfo(
+                                        new TypeInformation[] {Types.DOUBLE, Types.SQL_TIMESTAMP},
+                                        new String[] {"f1", "f2"})));
+        try {
+            TypeTransform transform =
+                    new TypeTransform()
+                            .setToStringCols("f1", "f3")
+                            .setKeepOldCols(true)
+                            .setDefaultStringValue("null");
+            Table output = transform.transform(inputTable)[0];
+            IteratorUtils.toList(tEnv.toDataStream(output).executeAndCollect());
+        } catch (Exception e) {
+            Assert.assertEquals(e.getMessage(), "Column: f3 doesn't exist in the input table.");
+        }
     }
 }
